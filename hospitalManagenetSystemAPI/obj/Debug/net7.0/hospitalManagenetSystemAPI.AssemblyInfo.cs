4257396ca63f5506b3233cf869c6a7faa9135312
AdrianIchiro <adrianfahren10@gmail.com>
2024-05-06 23:01:25 +0700

    first commit

--- hospitalManagenetSystemAPI/obj/Debug/net7.0/hospitalManagenetSystemAPI.AssemblyInfo.cs
+++ hospitalManagenetSystemAPI/obj/Debug/net7.0/hospitalManagenetSystemAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hospitalManagenetSystemAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61a01ff2b7e93eae050ba7ad37bf2bbd36bbd7cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("hospitalManagenetSystemAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hospitalManagenetSystemAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
