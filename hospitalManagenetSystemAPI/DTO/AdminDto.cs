7dd917040fb8aac6b3e383f03f536f08dfe4e677
KevZ12 <135513743+KevZ12@users.noreply.github.com>
2024-05-12 17:49:06 +0700

    menambahkan controller doctor dan admin

---
+++ hospitalManagenetSystemAPI/DTO/AdminDto.cs
@@ -0,0 +1,12 @@
+﻿namespace hospitalManagenetSystemAPI.DTO
+{
+    public class AdminDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ hospitalManagenetSystemAPI/DTO/DoctorDto.cs
@@ -0,0 +1,16 @@
+﻿namespace hospitalManagenetSystemAPI.DTO
+{
+    public class DoctorDto
+    {
+ 
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public string Address { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+       
+    }
+}
