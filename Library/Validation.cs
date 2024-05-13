4131232a068d7f519178189f1a2cf0318d50e3e2
sintext <116945857+1shtalica@users.noreply.github.com>
2024-05-13 21:37:04 +0700

    add validation schedule

---
+++ Library/Validation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public static class Validation
+    {
+        public static T checkType<T>(T value, Type expectedType)
+        {
+            try
+            {
+                if (value.GetType() != expectedType)
+                {
+                    throw new ArgumentException($"Error: Input Data Type {value.GetType()}, while the expected data type is {expectedType}.");
+                }
+                return value;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return default;
+            }
+        }
+
+        
+    }
+}
--- Library/obj/Debug/net7.0/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net7.0/Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72137f76d5b96dbe7670915f378eff3b6bc9e1c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
