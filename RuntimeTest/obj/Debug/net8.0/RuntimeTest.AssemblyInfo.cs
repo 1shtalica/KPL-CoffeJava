97faef5712696022dda2fd00c02af8dfe560c301
sintext <116945857+1shtalica@users.noreply.github.com>
2024-05-14 15:46:53 +0700

    menambahkan runtime configuration untuk bahasa

--- RuntimeTest/obj/Debug/net8.0/RuntimeTest.AssemblyInfo.cs
+++ RuntimeTest/obj/Debug/net8.0/RuntimeTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RuntimeTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e40777f1080f7af137edaf03910f17eddcb134e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f42b75564922d42610a4414d15eb0b42ca2a5df")]
 [assembly: System.Reflection.AssemblyProductAttribute("RuntimeTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RuntimeTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ hospitalManagenetSystemAPI/Runtime/DashboardConfigParse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace hospitalManagenetSystemAPI.Runtime
+{
+    public class DashboardConfigParse
+    {
+        public DashboardConfigParse readData()
+        {
+            string file = File.ReadAllText("E:\\Kuliah\\Tugas\\MATKUL\\Semester 4\\Konstruksi Perangkat Lunak\\TUBES\\Proggress#6\\KPL-CoffeJava\\hospitalManagenetSystemAPI\\Runtime\\DashboardConfig.json");
+            DashboardConfigParse data = JsonConvert.DeserializeObject<DashboardConfigParse>(file);
+            return data;
+        }
+
+        public class Id
+        {
+            public string dashboard {  get; set; }
+            public string patients {  get; set; }
+            public string appointments { get; set; }
+            public string medicalRecords { get; set; }
+            public string settings { get; set; }
+            public string logout { get; set; }
+        }
+
+        public Id id { get; set; }
+        public class En
+        {
+            public string dashboard { get; set; }
+            public string patients { get; set; }
+            public string appointments { get; set; }
+            public string medicalRecords { get; set; }
+            public string settings { get; set; }
+            public string logout { get; set; }
+        }
+
+        public En en { get; set; }
+
+    }
+
+        
+}
